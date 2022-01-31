f88de504e4693695fe86d283038791da73b84925
Will Moritz <willthefirst@gmail.com>
2022-01-31 09:03:14 -0800

    Delete line

---
+++ delete_line/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * Use command + arrow to go ends of line
+ * Use shift to select and delete
+ * Use command + K to delete
+ */
+export function delete_line() {
+  const please = "delete me";
+}
