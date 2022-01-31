514646a2b70847306ed370e560ec569eeab6ab45
Will Moritz <willthefirst@gmail.com>
2022-01-31 09:00:25 -0800

    Add move_line

---
+++ move_line/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * Use option + arrows to move lines up and down
+ * Use shift + arrows to select multiple to the move
+ */
+export function moveMyLines() {
+  console.log('Step 1');
+
+  console.log('Step 3');
+
+  console.log('Step 2');
+
+  console.log('Step 5');
+
+  console.log('Step 4');
+
+  console.log('Step 8');
+
+  console.log('Step 6');
+
+  console.log('Step 7');
+}
